aac0a80daa10b1495dfabf2f1f539dea57685c3a
systic2 <llsysticll@gmail.com>
2021-10-07 21:20:11 +0900

    10월 7일 ver.1

---
+++ src/main/java/ctrl/ListServlet.java
@@ -0,0 +1,9 @@
+package ctrl;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/bbs_list.kpc")
+public class ListServlet extends HttpServlet {
+
+}
